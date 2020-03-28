069b3d234a58d90e415cba61a05762b5100402c3
dff9th <dff9th@gmail.com>
2020-03-28 02:45:31 +0000

    Fix function ParseArguments

--- src/argparser.cpp
+++ src/argparser.cpp
@@ -1,7 +1,7 @@
 #include "errutil.hpp"
 
 extern void ParseArguments(const int argc, const char* const *argv) {
-    const int ARG_NUM {1};
+    const int ARG_NUM {3};
     if (argc != ARG_NUM) {
         throw ErrorCode::ERROR_INVALID_ARG;
     }
--- test/argparser_test.cpp
+++ test/argparser_test.cpp
@@ -3,34 +3,20 @@
 #include "argparser.hpp"
 #include "errutil.hpp"
 
-using std::string;
-using std::vector;
-
-namespace internal {
+TEST(ParseArguments, FewArgs) {
+    const int argc {2};
+    const char* const argv[argc+1] {"a.out", "outfile", 0};
+    EXPECT_THROW(ParseArguments(argc, argv), ErrorCode);
 }
 
-TEST(ParseArguments, InvalidArg) {
-    // Invalide arguments
-    {
-        const int argc {1};
-        const char* const argv[argc+1] {"a.out", 0};
-        EXPECT_THROW(ParseArguments(argc, argv), ErrorCode);
-    }
-    {
-        const int argc {2};
-        const char* const argv[argc+1] {"a.out", "outfile", 0};
-        EXPECT_THROW(ParseArguments(argc, argv), ErrorCode);
-    }
-    {
-        const int argc {4};
-        const char* const argv[argc+1] {"a.out", "outfile", "infile", "extra", 0};
-        EXPECT_THROW(ParseArguments(argc, argv), ErrorCode);
-    }
+TEST(ParseArguments, ManyArgs) {
+    const int argc {4};
+    const char* const argv[argc+1] {"a.out", "outfile", "infile", "extra", 0};
+    EXPECT_THROW(ParseArguments(argc, argv), ErrorCode);
+}
 
-    // Valid arguments
-    {
-        const int argc {3};
-        const char* const argv[argc+1] {"a.out", "outfile", "infile", 0};
-        EXPECT_NO_THROW(ParseArguments(argc, argv));
-    }
+TEST(ParseArguments, ValidArg) {
+    const int argc {3};
+    const char* const argv[argc+1] {"a.out", "outfile", "infile", 0};
+    EXPECT_NO_THROW(ParseArguments(argc, argv));
 }
