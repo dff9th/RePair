a0d973396b5d20ea9b49dc433eb7a76053bcad48
dff9th <dff9th@gmail.com>
2020-03-28 02:22:40 +0000

    Add test

--- test/argparser_test.cpp
+++ test/argparser_test.cpp
@@ -10,9 +10,27 @@ namespace internal {
 }
 
 TEST(ParseArguments, InvalidArg) {
-    // Invalide argc
+    // Invalide arguments
     {
-        char **argv {};
-        EXPECT_THROW(ParseArguments(0, argv), ErrorCode);
+        const int argc {1};
+        const char* const argv[argc+1] {"a.out", 0};
+        EXPECT_THROW(ParseArguments(argc, argv), ErrorCode);
+    }
+    {
+        const int argc {2};
+        const char* const argv[argc+1] {"a.out", "outfile", 0};
+        EXPECT_THROW(ParseArguments(argc, argv), ErrorCode);
+    }
+    {
+        const int argc {4};
+        const char* const argv[argc+1] {"a.out", "outfile", "infile", "extra", 0};
+        EXPECT_THROW(ParseArguments(argc, argv), ErrorCode);
+    }
+
+    // Valid arguments
+    {
+        const int argc {3};
+        const char* const argv[argc+1] {"a.out", "outfile", "infile", 0};
+        EXPECT_NO_THROW(ParseArguments(argc, argv));
     }
 }
